37d2e5a1840e20f4b8e87edee291977e17e539fc
Denzale Reese <denzale@denzale-macbookpro.roam.corp.google.com>
2019-01-08 21:01:43 -0800

    finishes play function and Phase 1 reqs

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -134,7 +134,17 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
     player = 0  # Which player is about to take a turn, 0 (first) or 1 (second)
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
-    
+    while(score0 < goal and score1 < goal):
+        if(player == 0):
+            num_rolls = strategy0(score0, score1)
+            score0 += take_turn(num_rolls, score1, dice)
+        else:
+            num_rolls = strategy1(score1, score0)
+            score1 += take_turn(num_rolls, score0, dice)
+        swap = is_swap(score0, score1)
+        if(swap):
+            score0, score1 = score1, score0
+        player = other(player)
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
